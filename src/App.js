acc5b3286aa87995bd370c57eafc78cc3888cdb2
pranav_j <pranav.jawaji_c@shoptimize.ai>
2021-06-30 16:37:19 +0530

    Tab component and hover logic added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
+function Tab({children}) {
+  const [highlightStyle, setHighlightStyle] = useState({left: 0, opacity: 0});
+
+  function hideHighlight(e) {
+    setHighlightStyle({
+      opacity: 0,
+      left: e.nativeEvent.layerX - 150,
+    });
+  }
+
+  function moveHighlight(e){
+    setHighlightStyle({
+      left: e.nativeEvent.layerX - 150,
+    });
+  }
+
+  return (
+    <div className="tab" onMouseOut={hideHighlight} onMouseMove={moveHighlight}>
+      <div className="highlight" style={highlightStyle}/>
+      {children}
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab">
-            <div className="highlight" />
+          <Tab>
             <a>Home</a>
-          </div>
-          <div className="tab">
+          </Tab>
+          <Tab>
             <a>About</a>
-          </div>
-          <div className="tab">
+          </Tab>
+          <Tab>
             <a>Features</a>
-          </div>
+          </Tab>
         </div>
 
         <div className="viewport">Pages Go Here</div>
