74f1671905090b2421090bc3cf17a46c69a95774
pranav_j <pranav.jawaji_c@shoptimize.ai>
2021-06-30 17:07:21 +0530

    Router logic added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import Tab from "./components/Tab"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Features from "./pages/Features"
 
 function App() {
   return (
-    <div className="app">
-      <div className="browser">
-        <div className="tabs">
-          <Tab>
-            <a>Home</a>
-          </Tab>
-          <Tab>
-            <a>About</a>
-          </Tab>
-          <Tab>
-            <a>Features</a>
-          </Tab>
-        </div>
+    <Router>
+      <div className="app">
+        <div className="browser">
+          <div className="tabs">
+            <Tab>
+            <Link to="/">Home</Link>
+            </Tab>
+            <Tab>
+              <Link to="/about">About</Link>
+            </Tab>
+            <Tab>
+              <Link to="/features">Features</Link>
+            </Tab>
+          </div>
 
-        <div className="viewport">Pages Go Here</div>
+          <div className="viewport">
+            <Switch>  
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/features">
+                <Features />
+              </Route>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </div>
       </div>
-    </div>
+      
+    </Router>
   );
 }
 
